5560fd95e2c250c72b0f4ba43355f329fa75c8f6
William <escriva.william@outlook.com>
2021-10-13 23:47:19 +0200

    initialisation de friday-back avec Spring Web,Spring JPA,Spring DevTools, H2 database. initialisation de friday-front avec AngularCLI

---
+++ friday-back/src/test/java/com/friday/fridayback/FridayBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friday.fridayback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridayBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
