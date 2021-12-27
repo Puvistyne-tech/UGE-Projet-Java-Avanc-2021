cb18dd29521fec0de8ad08ab6e23bc54fed440c5
WilliamEsc <87037842+WilliamEsc@users.noreply.github.com>
2021-12-28 00:43:20 +0100

    :construction:Details Todo quasi tout:construction:

--- friday-front/src/app/main/details/fiche-rdv/fiche-rdv.component.ts
+++ friday-front/src/app/main/details/fiche-rdv/fiche-rdv.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Events } from 'src/app/api-call.service';
 
 @Component({
   selector: 'app-fiche-rdv',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fiche-rdv.component.css']
 })
 export class FicheRdvComponent implements OnInit {
+  @Input()
+  events!: Events;
 
   constructor() { }
 
