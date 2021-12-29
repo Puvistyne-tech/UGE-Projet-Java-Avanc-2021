ca7d69cc1b2207cb6d3179dae49f01d59707da09
WilliamEsc <87037842+WilliamEsc@users.noreply.github.com>
2021-12-29 17:02:03 +0100

    :sparkles: Detail d'un evenement lors du click dessus :sparkles:

--- friday-front/src/app/main/details/list-rdv/list-rdv.component.ts
+++ friday-front/src/app/main/details/list-rdv/list-rdv.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Events } from 'src/app/api-call.service';
+import { Component, Input, OnInit , Output, EventEmitter} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { ApiCallService,Events } from 'src/app/api-call.service';
 
 @Component({
   selector: 'app-list-rdv',
@@ -7,11 +8,28 @@ import { Events } from 'src/app/api-call.service';
   styleUrls: ['./list-rdv.component.css']
 })
 export class ListRdvComponent implements OnInit {
+  constructor( private apiService : ApiCallService) { }
+  ngOnInit(): void {}
   @Input() events : Array<Events> = [];
+  @Output() clickForDetails = new EventEmitter();
 
-  constructor() { }
+  sendToParent(evt:Events){
+    
+    this.clickForDetails.emit(evt);
+  }
 
-  ngOnInit(): void {
+  form = new FormGroup({
+    title: new FormControl('', [Validators.minLength(2),Validators.required]),
+    description: new FormControl('', Validators.required),
+    location: new FormControl('', Validators.required),
+    allDay : new FormControl(false),
+    start: new FormControl(new Date().toISOString().slice(0, 16),Validators.required),
+    end : new FormControl(new Date().toISOString().slice(0, 16))
+  });
+  dateValid = true;
+
+  get title(): any {
+    return this.form.get('title');
   }
 
   select(id : number){
@@ -20,6 +38,38 @@ export class ListRdvComponent implements OnInit {
       elements[i].setAttribute("class","list-group-item list-group-item-action")
     }
     document.getElementById(id+"")?.setAttribute("class",document.getElementById(id+"")?.getAttribute("class") + " active");
+    this.sendToParent(this.events[id]);
+  }
+
+  validate(event : Events){
+    if(event.allDay && event.start.toString() != ''){
+      return true;
+    }
+    if(!event.allDay && event.start.toString() != ''&& event.end.toString() != ''){
+      return event.start < event.end;
+    }
+    return false;
+  }
+
+  onSubmit(){
+    let tmp = this.form.value;
+    let evt= <Events> {
+      title: tmp.title,
+      location: tmp.location,
+      description: tmp.description,
+      start:tmp.start,
+      end: tmp.allDay? tmp.start : tmp.end,
+      allDay:tmp.allDay
+    };
+    this.dateValid = this.validate(evt);
+    if(this.dateValid){
+      this.apiService.postEvent(evt).subscribe(reponse => {
+        if(reponse.status == 200){
+          this.apiService.sendClickEvent();
+        }
+        
+      })
+    }
   }
 
 }
