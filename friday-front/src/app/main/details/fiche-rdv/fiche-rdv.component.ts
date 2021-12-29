c9e131b13536b827d105068fe34e3a0191156c20
WilliamEsc <87037842+WilliamEsc@users.noreply.github.com>
2021-12-29 16:57:28 +0100

    :sparkles: Ajout de la possibilité d'update et delete un event :sparkles:

--- friday-front/src/app/main/details/fiche-rdv/fiche-rdv.component.ts
+++ friday-front/src/app/main/details/fiche-rdv/fiche-rdv.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Events } from 'src/app/api-call.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { ApiCallService,Events } from 'src/app/api-call.service';
 
 @Component({
   selector: 'app-fiche-rdv',
@@ -10,9 +11,61 @@ export class FicheRdvComponent implements OnInit {
   @Input()
   events!: Events;
 
-  constructor() { }
+  form = new FormGroup({
+    title: new FormControl('', [Validators.minLength(2),Validators.required]),
+    description: new FormControl('', Validators.required),
+    location: new FormControl('', Validators.required),
+    allDay : new FormControl(false),
+    start: new FormControl(Validators.required),
+    end : new FormControl()
+  });
+  dateValid = true;
+
+  get title(): any {
+    return this.form.get('title');
+  }
+
+  constructor(private apiService : ApiCallService) { }
 
   ngOnInit(): void {
+  }
+
+  delete(){
+    this.apiService.delEvent(this.events.id!).subscribe(response =>{
+      console.log(response);
+      this.apiService.sendClickEvent();
+    });
+  }
+
+  validate(event : Events){
+    if(event.allDay && event.start.toString() != ''){
+      return true;
+    }
+    if(!event.allDay && event.start.toString() != ''&& event.end.toString() != ''){
+      return event.start < event.end;
+    }
+    return false;
+  }
+
+  onSubmitUpdate(){
+    let tmp = this.form.value;
+    let evt= <Events> {
+      id: this.events.id,
+      title: tmp.title,
+      location: tmp.location,
+      description: tmp.description,
+      start:tmp.start,
+      end: tmp.allDay? tmp.start : tmp.end,
+      allDay:tmp.allDay
+    };
+    this.dateValid = this.validate(evt);
+    console.log(evt);
+    if(this.dateValid){
+      this.apiService.postEvent(evt).subscribe(date => {
+        console.log(date);
+        this.apiService.sendClickEvent();
+      })
+    }
   }
 
 }
