0fc7a6dbb0532bc3f74f873ea08b4809e8982e69
WilliamEsc <87037842+WilliamEsc@users.noreply.github.com>
2021-12-29 17:06:00 +0100

    :label: Events correspondant aux events de full calendar :label: :sparkles: possibilité de reload tout les events :sparkles:

--- friday-front/src/app/api-call.service.ts
+++ friday-front/src/app/api-call.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; 
+import { Observable, Subject } from 'rxjs';
 
 export interface Events{
-    id:string,
-    name:string,
+    id?:string,
+    title:string,
     description:String,
-    startDate:Date,
-    endDate:Date,
-    startTime:String,
-    endTime:String
+    location:string,
+    start:Date,
+    end:Date,
+    allDay?:boolean
 }
 
 @Injectable({
@@ -17,14 +18,37 @@ export interface Events{
 export class ApiCallService {
 
   constructor(private http : HttpClient) { }
+  private subject = new Subject<any>();
 
   getAllEvents(){
     return this.http.get<Events[]>("/api/events/event",
-    {observe: 'response'});
+                                    {observe: 'response'});
   }
 
   getEventsByDate(date:string){
     return this.http.get<Events[]>("/api/events/findByDate?date="+date,
-    {observe: 'response'});
+                                    {observe: 'response'});
+  }
+
+  postEvent(evt:Events){
+    return this.http.post<Event>("/api/events/event",evt,
+                                  {observe: 'response'});
+  }
+
+  putEvent(evt:Events){
+    return this.http.put<Event>("/api/events/event",evt,
+                                  {observe: 'response'});
+  }
+
+  delEvent(id:string){
+    return this.http.delete<number>("/api/events/"+id);
+  }
+
+  sendClickEvent() {
+    this.subject.next();
+  }
+
+  reloadEvent(): Observable<any>{ 
+    return this.subject.asObservable();
   }
 }
