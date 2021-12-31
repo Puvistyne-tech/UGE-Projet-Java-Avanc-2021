252ef688e72a3d56ea5f844cfced08f764a5c196
WilliamEsc <87037842+WilliamEsc@users.noreply.github.com>
2021-12-31 03:57:54 +0100

    :fire: Removed commented code :fire:

--- friday-back/src/main/java/com/friday/fridayback/repository/EventRepository.java
+++ friday-back/src/main/java/com/friday/fridayback/repository/EventRepository.java
@@ -3,7 +3,6 @@ package com.friday.fridayback.repository;
 import com.friday.fridayback.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -17,13 +16,5 @@ public interface EventRepository
     @Query("select d from Event d where d.start >= ?1 and d.start < ?2")
     List<Event> findEventByStartDate(LocalDateTime startDate, LocalDateTime endDate);
 
-//    List<Event> findEventsByStartDateIsGreaterThanEqualAndAndEndDateLessThanEqual(Date date);
-
     Optional<Event> findByTitle(String title);
-
-//    public List<Event> findAllByEndDateOrStartDate(Date date);
-
-//    @Modifying
-//    @Query("update Event u se")
-//    public void updateEvent()
 }
